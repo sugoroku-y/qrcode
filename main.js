58a6c2f1c77ca053b0a52873d6d21ddef65e3fe3
YEBISUYA Sugoroku <yebisuya@gmail.com>
2020-09-19 16:12:01 +0900

    同じ処理を行っているか所を関数化

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-window.addEventListener('load', function () {
-  const qrcode = new QRCode("qrcode", {text: text.value});
-  text.addEventListener('input', _ => qrcode.makeCode(text.value));
+function updateWifi() {
+  wifi.textContent = `WIFI:S:${ssid.value};T:${securetype.value};P:${password.value};;`;
+}
+
+window.addEventListener('load', () => {
+  const qrcode = new QRCode('qrcode', {text: text.value});
+  text.addEventListener('input', () => qrcode.makeCode(text.value));
   wifi.addEventListener('click', () => {
     text.value = wifi.textContent;
     qrcode.makeCode(text.value);
   });
-  ssid.addEventListener('input', () => wifi.textContent = `WIFI:S:${ssid.value};T:${securetype.value};P:${password.value};;`)
-  securetype.addEventListener('change', () => wifi.textContent = `WIFI:S:${ssid.value};T:${securetype.value};P:${password.value};;`)
-  password.addEventListener('input', () => wifi.textContent = `WIFI:S:${ssid.value};T:${securetype.value};P:${password.value};;`)
+  ssid.addEventListener('input', updateWifi);
+  securetype.addEventListener('change', updateWifi);
+  password.addEventListener('input', updateWifi);
 });
